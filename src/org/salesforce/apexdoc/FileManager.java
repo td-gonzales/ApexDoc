2d3d2b2f4f230c6f5c77bf6d4c39b2a288640c3f
TD Gonzales <tdgonzales@vlocity.com>
2015-08-05 10:03:34 -0700

    Disabled html encoding so rich documentation can be made.

--- src/org/salesforce/apexdoc/FileManager.java
+++ src/org/salesforce/apexdoc/FileManager.java
@@ -30,13 +30,7 @@ public class FileManager {
         StringBuilder out = new StringBuilder(Math.max(16, s.length()));
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c > 127 || c == '"' || c == '<' || c == '>' || c == '&') {
-                out.append("&#");
-                out.append((int) c);
-                out.append(';');
-            } else {
-                out.append(c);
-            }
+            out.append(c);
         }
         return out.toString();
     }
